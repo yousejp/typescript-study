a1466dafc2d31c2850099ad85022038498221021
yousejp <yousejp25@gmail.com>
2020-09-07 16:06:08 +0900

    never ・objectの勉強

--- src/never.ts
+++ src/never.ts
@@ -3,12 +3,20 @@ export {};
 function error(message: string): never {
   throw new Error(message);
 }
+//関数の中で例外を起こす場合neverを使用する
+//voidは呼び元に戻るがneverは戻らない
+//実務では使う場面がないのかも？
 
 try {
   let result = error('test');
   console.log({ result });
+  //上記のconsole.logは実行されない
+
+  console.log('try');
 } catch (error) {
   console.log({ error });
+
+  console.log('catch');
 }
 
 let foo: void = undefined;
--- src/object.ts
+++ src/object.ts
@@ -2,8 +2,22 @@ export {};
 
 let profile1: object = { name: 'Ham' };
 profile1 = { brithYear: 1976 };
+//objectで上書きできてしまうので制約が甘い
+
+// let profile2 = { name: 'Nimo' };
+//ここで型推論が出る
 
 let profile2: {
   name: string;
 } = { name: 'Ham' };
 profile2 = { name: 'Nimo' };
+
+let profile3: {
+  name: string;
+  num: number;
+} = {
+  name: 'Ham',
+  num: 1,
+};
+profile3.num += 2;
+console.log(profile3.num);
