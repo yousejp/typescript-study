c9ff6b0a09a5257bf7b312d19fa8a0921cf6a385
yousejp <yousejp25@gmail.com>
2020-09-07 22:54:19 +0900

    unknown・intersectionの勉強

--- src/unknown.ts
+++ src/unknown.ts
@@ -6,6 +6,13 @@ let numberAny: any = kansu();
 let numberUnknown: unknown = kansu();
 
 let sumAny = numberAny + 10;
+console.log(typeof numberUnknown);
+
+// let sumUnknown = numberUnknown + 10;
+// unknownだと数値じゃないかもしれないのでエラー
+
 if (typeof numberUnknown === 'number') {
   let sumUnknown = numberUnknown + 10;
+  console.log(sumUnknown);
 }
+//型を調べて安全を図ることをtypeガードという
