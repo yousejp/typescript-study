8f3fc71305b8830a91823911c72e569d67a4c279
yousejp <yousejp25@gmail.com>
2020-09-09 15:54:48 +0900

    parameterとoverloadsの勉強

--- src/overloads.ts
+++ src/overloads.ts
@@ -2,8 +2,10 @@ export {};
 
 function double(value: number): number;
 function double(value: string): string;
+//上のものをシグネチャーといい、両方の値を受け付ける
 
 function double(value: any): any {
+  //any型なのは、union型で指定する必要はなく、上のシグネチャーで定義している
   console.log(typeof value);
   if (typeof value === 'number') {
     return value * 2;
--- src/rest-parameters.ts
+++ src/rest-parameters.ts
@@ -1,12 +1,21 @@
 export {};
 
-const reducer = (accumulator: number, currentValue: number) => {
+const reducer = (accumulator: number, currentValue: number): number => {
   console.log({ accumulator, currentValue });
   return accumulator + currentValue;
 };
+//配列の全ての数currentValueを足していっているのがaccumulator
+// [1, 2, 3].reduce(reducer);
 
 const sum: (...values: number[]) => number = (...values: number[]): number => {
   return values.reduce(reducer);
 };
 
 console.log(sum(1, 2, 3, 4, 5));
+
+const sumTest: (...values: number[]) => number = (
+  ...values: number[]
+): number => {
+  // console.log(values);
+  return 100;
+};
