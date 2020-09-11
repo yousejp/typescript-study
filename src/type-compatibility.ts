dd94d1e0bdfc1b1016fe99ada66428ce1c4ba824
yousejp <yousejp25@gmail.com>
2020-09-12 04:34:42 +0900

    型の互換性とジェネリクスの勉強

--- src/type-compatibility.ts
+++ src/type-compatibility.ts
@@ -18,13 +18,16 @@ let fooString: string;
 let barString: string = 'string';
 
 fooString = barString;
+console.log(fooString, 'stringです');
 
 let fooStringLiteral: 'fooStringLiteral' = 'fooStringLiteral';
 fooString = fooStringLiteral;
+console.log(fooString, 'fooStringLiteralです');
 
 let fooNumber: number;
 let fooNumberLiteral: 1976 = 1976;
 fooNumber = fooNumberLiteral;
+console.log(typeof fooNumber, fooNumber, '1976です');
 
 interface Animal {
   age: number;
