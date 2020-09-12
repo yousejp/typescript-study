304b9e422ddee7a9178a04b85fa8416ec9e32f97
yousejp <yousejp25@gmail.com>
2020-09-12 17:54:28 +0900

    アサーションの勉強

--- src/as-const.ts
+++ src/as-const.ts
@@ -5,12 +5,16 @@ let name = 'Atsushi';
 name = 'Ham';
 
 let nickname = 'Ham' as const;
+
+// nickname = 'Hamtaro';
+// 'Ham'型になっているため'Ham'以外は代入できない
 nickname = 'Ham';
 
 let profile = {
   name: 'Atsushi',
-  height: 178
+  height: 178,
 } as const;
+// 全ての要素がreadonlyになる
 
 // profile.name = 'Ham';
 // profile.height = 180;
